06299ba2ed46be0700f88029f697218b700f5b32
Syed Aman <amanullahnaqvi@gmail.com>
2022-03-11 19:21:31 +0500

    Completed practice

--- practic/src/App.js
+++ practic/src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React ,  {useState , useEffect} from 'react';
 import './App.css'
 
-const Person = ({name, age , imageUrl , height}) => {
-    return (
-        <div className='person_container'>
-            <h2>{name}</h2>
-            <h2>{age}</h2>
-            <img src={imageUrl} alt={name}/>
-            <h4>{height}</h4>
-        </div>
 
-    )
-}
+
+// Reusable Components
+
+// const Person = ({name, age , imageUrl , height}) => {
+//     return (
+//         <div className='person_container'>
+//             <h2>{name}</h2>
+//             <h2>{age}</h2>
+//             <img src={imageUrl} alt={name}/>
+//             <h4>{height}</h4>
+//         </div>
+
+//     )
+// }
 
 
 
@@ -19,17 +23,40 @@ const Person = ({name, age , imageUrl , height}) => {
 
 
 const App = () => {
+
+    // Dynamic transitions
     // const message = 'hi'
     // const user = "aman"
     // const isOnline = false;
      
+
+    // Using JS Fragments
     // const name = 'billie';
 
 
+
+
+    //Intro to UseState
+
+    // const [counter , setCounter] = useState(0);
+
+
+
+
+    // intro to useeffect
+    // useEffect(() => {
+    //    alert('You have changeed the counter to ' + counter)
+    // },[counter]);
     return(
+        // Dynamic transitions
         // <div className="App">
         //     <h1> hellooo {isOnline ? user : 'NOT ACTIVE'} </h1>
         // </div>
+       
+       
+       
+       
+        // Using JS fragments       
         //  <div className='App'>
         //     {name ? (
         //         <>
@@ -42,32 +69,45 @@ const App = () => {
         //         </>
         //     )}
         //  </div>
-         <div className='App'>
+        
+        
+        
+        // <div className='App'>
              
-            <Person 
-               name="Name:Aman"
-               age="Age:21"
-               imageUrl="https://images.unsplash.com/photo-1625838144804-300f3907c110?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-               height="Height:180 cm"
-             />
+        //     <Person 
+        //        name="Name:Aman"
+        //        age="Age:21"
+        //        imageUrl="https://images.unsplash.com/photo-1625838144804-300f3907c110?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+        //        height="Height:180 cm"
+        //      />
              
-             <Person 
-               name="Sergio"
-               age="21"
-               imageUrl="https://images.unsplash.com/photo-1625838144804-300f3907c110?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-               height="180 cm"
-             />
+        //      <Person 
+        //        name="Sergio"
+        //        age="21"
+        //        imageUrl="https://images.unsplash.com/photo-1625838144804-300f3907c110?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+        //        height="180 cm"
+        //      />
 
-              <Person 
-               name="Sameer"
-               age="21"
-               imageUrl="https://images.unsplash.com/photo-1625838144804-300f3907c110?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-               height="180 cm"
-             />
-        </div>
+        //       <Person 
+        //        name="Sameer"
+        //        age="21"
+        //        imageUrl="https://images.unsplash.com/photo-1625838144804-300f3907c110?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+        //        height="180 cm"
+        //      />
+        // </div>
             
 
 
+        //Intro to UseState
+        // <div className='App'>
+        //      <button onClick= { () => setCounter ((prevCount) => prevCount - 1)}>-</button>
+        //      <h1>{counter}</h1>
+        //      <button onClick = { () => setCounter ((prevCount) => prevCount + 1)}>+</button>
+        // </div>
+
+          
+
+        <div className='App'></div>
 
 
 
